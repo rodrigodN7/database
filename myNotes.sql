3bbba914f7d9bdc850eed7291b93e7ddb21cf0fa
root <rodrigod.noriega@gmail.com>
2023-05-10 10:04:26 -0600

    my notes

--- myNotes.sql
+++ myNotes.sql
@@ -51,3 +51,38 @@ e.g.
 | comission       | tinyint(4)  | YES  |     | NULL    |       |
 +-----------------+-------------+------+-----+---------+-------+
 
+#Insert data in to table
+
+insert into sales_rep(employee_number,surname,first_name,comission) values(1,'Rive','Sol','10');
+insert into sales_rep(employee_number,surname,first_name,comission) values(2,'Gordimer','Charlene','15');
+insert into sales_rep(employee_number,surname,first_name,comission) values(3,'Serote','Mike','10');
+
+#insertar datos desde un archivo de texto con LOAD DATA
+#el formato debe ser .csv utf-8, 
+e.g.
+
+> LOAD DATA LOCAL INFILE '/var/tmp/eg.csv' replace into table sales_rep fields terminated by ',' lines terminated by '\n';
+
+Query OK, 4 rows affected (0.01 sec)
+Records: 4  Deleted: 0  Skipped: 0  Warnings: 0
+
+#using select to get informaton from table
+
+e.g.
+
+select * from sales_rep;
++-----------------+----------+------------+-----------+
+| employee_number | surname  | first_name | comission |
++-----------------+----------+------------+-----------+
+|               1 | Rive     | Sol        |        10 |
+|               2 | Gordimer | Charlene   |        15 |
+|               3 | Serote   | Mike       |        10 |
++-----------------+----------+------------+-----------+
+
+#Delete complete data from a table without deleting the table structure
+truncate table sales_rep
+
+#insertar datos desde un archivo de texto con LOAD DATA
+#el formato debe ser .csv utf-8,
+e.g.
+
