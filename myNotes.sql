67c0e9202d97e9a12928410388ff59329228bdbc
root <rodrigod.noriega@gmail.com>
2023-05-10 12:59:29 -0600

    myNotes

--- myNotes.sql
+++ myNotes.sql
@@ -66,14 +66,14 @@ e.g.
 
 > LOAD DATA LOCAL INFILE '/var/tmp/eg.csv' replace into table sales_rep fields terminated by ',' lines terminated by '\n';
 
-Query OK, 4 rows affected (0.01 sec)
-Records: 4  Deleted: 0  Skipped: 0  Warnings: 0
+Query OK, 3 rows affected (0.01 sec)
+Records: 3  Deleted: 0  Skipped: 0  Warnings: 0
 
-#using select to get informaton from table
+#using select to get/extract informaton from table
 
 e.g.
 
-select * from sales_rep;
+>select * from sales_rep;
 +-----------------+----------+------------+-----------+
 | employee_number | surname  | first_name | comission |
 +-----------------+----------+------------+-----------+
@@ -81,4 +81,82 @@ select * from sales_rep;
 |               2 | Gordimer | Charlene   |        15 |
 |               3 | Serote   | Mike       |        10 |
 +-----------------+----------+------------+-----------+
+
+>select comission from sales_rep where surname='Gordimer';
++-----------+
+| comission |
++-----------+
+|        15 |
++-----------+
+1 row in set (0.00 sec)
+
+>select comission,employee_number from sales_rep where surname='Gordimer';
++-----------+-----------------+
+| comission | employee_number |
++-----------+-----------------+
+|        15 |               2 |
++-----------+-----------------+
+1 row in set (0.00 sec)
+
+>select * from sales_rep where surname='Gordimer';
++-----------------+----------+------------+-----------+
+| employee_number | surname  | first_name | comission |
++-----------------+----------+------------+-----------+
+|               2 | Gordimer | Charlene   |        15 |
++-----------------+----------+------------+-----------+
+1 row in set (0.01 sec)
+
+>select * from sales_rep where comission>10;
++-----------------+----------+------------+-----------+
+| employee_number | surname  | first_name | comission |
++-----------------+----------+------------+-----------+
+|               1 | Rodrigo  | Noriega    |       117 |
+|               2 | Gordimer | Charlene   |        15 |
++-----------------+----------+------------+-----------+
+2 rows in set (0.00 sec)
+
+>select * from sales_rep where surname='Rive' and (first_name='Sol' or comission>10);
++-----------------+---------+------------+-----------+
+| employee_number | surname | first_name | comission |
++-----------------+---------+------------+-----------+
+|               3 | Rive    | Sol        |        10 |
++-----------------+---------+------------+-----------+
+1 row in set (0.00 sec)
+
+#correspondencias de patrones
+>select * from sales_rep where surname LIKE 'Ri%';
++-----------------+---------+------------+-----------+
+| employee_number | surname | first_name | comission |
++-----------------+---------+------------+-----------+
+|               3 | Rive    | Sol        |        10 |
++-----------------+---------+------------+-----------+
+1 row in set (0.00 sec)
+
+>select * from sales_rep where surname like '%e%';
++-----------------+----------+------------+-----------+
+| employee_number | surname  | first_name | comission |
++-----------------+----------+------------+-----------+
+|               2 | Gordimer | Charlene   |        15 |
+|               3 | Rive     | Sol        |        10 |
++-----------------+----------+------------+-----------+
+2 rows in set (0.00 sec)
+
+>select * from sales_rep where surname like 'g%';
++-----------------+----------+------------+-----------+
+| employee_number | surname  | first_name | comission |
++-----------------+----------+------------+-----------+
+|               2 | Gordimer | Charlene   |        15 |
++-----------------+----------+------------+-----------+
+1 row in set (0.00 sec)
+
+#search for surname which contains an o in some plase and finishes with o.
+#busca en surname que contenga o en alguna parte y que termine en o
+>select * from sales_rep where surname like '%o%o';
++-----------------+---------+------------+-----------+
+| employee_number | surname | first_name | comission |
++-----------------+---------+------------+-----------+
+|               1 | Rodrigo | Noriega    |       117 |
++-----------------+---------+------------+-----------+
+1 row in set (0.00 sec)
+
 
